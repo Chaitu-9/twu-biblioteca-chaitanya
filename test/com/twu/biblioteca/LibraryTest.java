20e6f882ce07b40518c1e35b9ee84fea01def931
Chaitanya Varma <chaitany@thoughtworks.com>
2015-06-15 15:02:40 +0530

    Added spec for showing available books in library

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,11 +1,14 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.Library;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class LibraryTest {
@@ -18,5 +21,22 @@ public class LibraryTest {
         int actual = library.availableBooksCount();
 
         assertThat(actual, is(equalTo(0)));
+    }
+
+    @Test
+    public void showAvailableBooksinLibrary() {
+        ArrayList<Book> booksList = new ArrayList<Book>();
+        booksList.add(new Book("Harry Potter", "J.K.Rowling", "1990"));
+        booksList.add(new Book("The Hobbit", "Tolkein", "2001"));
+        booksList.add(new Book("Hound of Baskervilles", "Doyle", "1902"));
+        Library library = new Library(booksList);
+
+        ArrayList<Book> actualBooksList = library.availableListOfBooks();
+        ArrayList<Book> expectedBooksList = new ArrayList<Book>();
+        expectedBooksList.add(new Book("Harry Potter", "J.K.Rowling", "1990"));
+        expectedBooksList.add(new Book("The Hobbit", "Tolkein", "2001"));
+        expectedBooksList.add(new Book("Hound of Baskervilles", "Doyle", "1902"));
+
+        assertThat(actualBooksList, is(expectedBooksList));
     }
 }
