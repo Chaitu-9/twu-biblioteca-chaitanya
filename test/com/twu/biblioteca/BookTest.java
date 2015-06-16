dcb26a72864cd86e74ed1273cd2cdd2ba9ffdf9f
Chaitanya Varma <chaitany@thoughtworks.com>
2015-06-16 18:43:40 +0530

    Added live template for checking equality in Books Class

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -18,5 +18,64 @@ public class BookTest {
 
         assertThat(actualString,is(book.toString()));
     }
+    @Test
+    public void testForReflexivity() {
+        Book firstObject= new Book(null,null,null);
 
+        assertEquals(firstObject, firstObject);
     }
+
+    @Test
+    public void testForComparingNullObjectsWithABook() {
+        Book firstObject= new Book(null,null,null);
+
+
+        assertThat(firstObject, not(equalTo(null)));
+    }
+
+    @Test
+    public void testForEquality() {
+        Book firstObject = new Book(null,null,null);
+        Book secondObject = new Book(null,null,null);
+
+        assertEquals(firstObject, secondObject);
+    }
+
+    @Test
+    public void testForDifferentObjects() {
+        Book firstObject= new Book(null,null,null);
+        Object one = new Integer(1);
+
+        assertThat(firstObject, not(equalTo(one)));
+    }
+
+    @Test
+    public void testForTransitivity() {
+        Book firstObject= new Book(null,null,null);
+        Book secondObject= new Book(null,null,null);
+        Book thirdObject= new Book(null,null,null);
+
+        assertEquals(firstObject, secondObject);
+        assertEquals(secondObject, firstObject);
+        assertEquals(firstObject, thirdObject);
+    }
+
+    @Test
+    public void testForSymmetry() {
+        Book firstObject= new Book(null,null,null);
+        Book secondObject= new Book(null,null,null);
+
+        assertEquals(firstObject, secondObject);
+        assertEquals(secondObject, firstObject);
+    }
+
+    @Test
+    public void equalityTestForHashCode() {
+        Book firstObject= new Book(null,null,null);
+        Book secondObject= new Book(null,null,null);
+
+        assertEquals(firstObject.hashCode(), secondObject.hashCode());
+    }
+
+
+}
